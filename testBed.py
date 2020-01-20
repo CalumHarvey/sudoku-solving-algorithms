68d40ce862c96c94dd85bcd0331cd7c0660cc45d
Calum Harvey <calum.harvey@blueyonder.co.uk>
2020-01-20 15:45:05 +0000

    update 2

--- testBed.py
+++ testBed.py
@@ -2,7 +2,8 @@
 
 
 #Selection of algorithm - use use input from user (give each algorithm a number 1-6)
-def algorithmSelection:
+@staticmethod
+def algorithmSelection(parameter_list):
 
     print("a description of each algorithm and how to select them all")
 
@@ -12,20 +13,24 @@ def algorithmSelection:
 
 
 
+def board(self, parameter_list):
 
 
 #Generation of board - potentially use pre-existing boards for initial design and potentially write creation algorithm at the end
 #Should call the board generation function in another file
 #look at c++ code and also s6 project (improve s6 project version) for creating the sudoku board but create new algorithm for the numbers
-def boardGen:
+@staticmethod
+def boardGen(parameter_list):
+    pass
 
-
+@staticmethod
 def sudokuGen:
 
 
 
 #Running the selected algorithm (main)
 #Can have more functions for data processing for time taken and number of passes through
+@staticmethod
 def runAlgorithm:
 
  
