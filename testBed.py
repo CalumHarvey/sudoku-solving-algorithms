05566d8bf2643dd604ac507f141b40a9d98e56d5
Calum Harvey <calum.harvey@blueyonder.co.uk>
2020-01-20 13:15:15 +0000

    outline of functions

---
+++ testBed.py
@@ -0,0 +1,34 @@
+
+
+
+#Selection of algorithm - use use input from user (give each algorithm a number 1-6)
+def algorithmSelection:
+
+    print("a description of each algorithm and how to select them all")
+
+    selectionAlgorithm = input("Select algorithm: ")
+
+    return selectionAlgorithm
+
+
+
+
+
+#Generation of board - potentially use pre-existing boards for initial design and potentially write creation algorithm at the end
+#Should call the board generation function in another file
+#look at c++ code and also s6 project (improve s6 project version) for creating the sudoku board but create new algorithm for the numbers
+def boardGen:
+
+
+def sudokuGen:
+
+
+
+#Running the selected algorithm (main)
+#Can have more functions for data processing for time taken and number of passes through
+def runAlgorithm:
+
+ 
+
+
+
