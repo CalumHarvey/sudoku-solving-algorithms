4e2cf99b59da4bfa44ab9ca15f76176c038b3635
Calum Harvey <calum.harvey@blueyonder.co.uk>
2020-02-19 12:29:01 +0000

    bug fixes to simannealing

--- algorithms/simannealing.py
+++ algorithms/simannealing.py
@@ -75,7 +75,7 @@ class SudokuSolve(Annealer):
                 colSet.add(self.state[y][x])
 
             score += (9 - len(rowSet))
-            score += (9 - len(colSet))           
+            score += (9 - len(colSet))         
 
         if(score == 0):
             self.user_exit = True
@@ -84,8 +84,11 @@ class SudokuSolve(Annealer):
 
 def runAlgorithm():
     board = np.array([[0, 0, 2, 1, 0, 0, 0, 0, 0], [7, 1, 0, 6, 0, 0, 0, 4, 0], [5, 0, 0, 9, 0, 3, 0, 0, 1], [2, 0, 0, 8, 0, 0, 0, 9, 3], [0, 8, 0, 0, 0, 0, 0, 1, 0], [9, 5, 0, 0, 0, 1, 0, 0, 4], [3, 0, 0, 4, 0, 9, 0, 0, 8], [0, 9, 0, 0, 0, 2, 0, 7, 6], [0, 0, 0, 0, 0, 7, 4, 0, 0]])
-    sudoku = SudokuSolve(board)
+    boardTest = np.array([[4, 6, 2, 1, 2, 8, 8, 7, 3,],[7, 1, 3, 6, 5, 7, 9, 4, 2],[5, 8, 9, 9, 4, 3, 5, 6, 1],[2, 6, 3, 8, 2, 3, 8, 9, 3],[1, 8, 4, 9, 5, 7, 7, 1, 5],[9, 5, 7, 4, 6, 1, 6, 2, 4],[3, 4, 5, 4, 8, 9, 3, 5, 8],[7, 9, 8, 6, 3, 2, 1, 7, 6],[1, 6, 2, 5, 1, 7, 4, 9, 2]])
+    sudoku = SudokuSolve(boardTest)
     sudoku.copy_strategy = "method"
+    #print(board)
+    #print(intialSolution(board))
 
     #sudoku.steps = 1000000
     #auto_schedule = sudoku.auto(minutes=1)
@@ -93,10 +96,10 @@ def runAlgorithm():
     #sudoku.set_schedule(auto_schedule)
 
 
-    sudoku.Tmax = 5
+    sudoku.Tmax = 10
     sudoku.Tmin = 0.05
-    sudoku.steps = 10000
-    sudoku.updates = 100
+    sudoku.steps = 700000
+    sudoku.updates = 1000
 
     #print(sudoku.board)
 
