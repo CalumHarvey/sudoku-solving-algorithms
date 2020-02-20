0c5e67b914698f13d406ee298d74baef4e382c3d
Calum Harvey <calum.harvey@blueyonder.co.uk>
2020-02-20 13:47:05 +0000

    bug fixes to generation
    removes cells but counter showing number of solutions for puzzle does not work

--- sudokuGen.py
+++ sudokuGen.py
@@ -1,53 +1,17 @@
-'''
-Initial design is to have preset puzzles at different difficulties
-difficulty can then be chosen and a random puzzle of that difficulty is chosen by the program
-'''
-
-difficultiesDict = {"1" : "easy", "2" : "medium", "3" : "hard"}
-
-import random
-import os
-
-
-currentDir = os.path.dirname(__file__)
-
-def boardPicker(difficulty):
-    finalBoard = []
-    
-    #lineNumber = random.randint(0,3)
-    lineNumber = 0 #remove when all boards implemented
-    filePath = "puzzles/" + difficultiesDict[difficulty] + ".txt"
-    absFilePath = os.path.join(currentDir, filePath)
-
-    f = open(absFilePath, "r")
-    allLines = f.readlines()
-    line = allLines[lineNumber]
-
-    lineStripped = line.strip()
-    firstSplit = lineStripped.split(",")
-
-    for x in firstSplit:
-        splitDone = list(map(int, x.split(" ")))
-        finalBoard.append(splitDone)
-    
-    return finalBoard
-
-boardPicker("1")
 
 '''
-
-BREAK
 1. Create valid completed board
 2. By selecting difficulty - chooses number of numbers to remove
+    easy = 10, medium = 20, hard = 30
 3. removes that number of cells, checking that there is only one solution
+    remove cell, check one solution
 4. when multiple solutions, but cell back and return puzzle
-
-
 
 '''
 
 import time
 import datetime
+import random
 
 def findUnassignedLocation(board, pos):
     for row in range(9):
@@ -82,7 +46,34 @@ def usedInBox(board, boxStartRow, boxStartCol, num):
                 return True
     return False
 
-def solveSudoku(board):
+def solveSudoku(board, counter):
+
+    pos = [0,0]
+    
+    if(not findUnassignedLocation(board, pos)):
+        print("solution Found")
+        counter += 1
+        return True
+
+    #might need this
+    row = pos[0]
+    col = pos[1]
+    
+    for num in range(9):
+        if(noConflicts(board, pos, num)):
+            board[row][col] = num
+
+            if(solveSudoku(board,counter)):
+                print("solution Found 2")
+                counter += 1
+                return True
+            
+            board[row][col] = 0
+    
+    return False
+
+
+def fillSudoku(board):
 
     numbers = [1,2,3,4,5,6,7,8,9]
     random.shuffle(numbers)
@@ -100,12 +91,70 @@ def solveSudoku(board):
         if(noConflicts(board, pos, num)):
             board[row][col] = num
 
-            if(solveSudoku(board)):
+            if(fillSudoku(board)):
                 return True
             
             board[row][col] = 0
     
     return False
+
+def difficultySelection(difficulty):
+    cellsToRemove = 0
+    if(difficulty == "1"):
+        cellsToRemove = 10
+    elif(difficulty ==  "2"):
+        cellsToRemove = 20
+    else:
+        cellsToRemove = 30
+    
+    return cellsToRemove
+
+
+def selectEmptyCell(grid):
+    row = random.randint(0,8)
+    col = random.randint(0,8)
+
+    while(grid[row][col] == 0):
+        row = random.randint(0,8)
+        col = random.randint(0,8)
+     
+    return row,col
+
+def main(grid):
+    attempts = 5
+    counter = 0
+
+    while attempts > 0:
+
+        coords = selectEmptyCell(grid)
+        backup = grid[coords[0]][coords[1]]
+
+        print("coords", coords)
+        print("before", grid)
+
+        grid[coords[0]][coords[1]] = 0
+        print("\n")
+
+        print("after", grid)
+        input()
+
+        copyGrid = []
+        for r in range(0,9):
+            copyGrid.append([])
+            for c in range(0,9):
+                copyGrid[r].append(grid[r][c])
+    
+        solveSudoku(copyGrid,counter)
+
+        print(counter)
+
+        if(counter != 1):
+            grid[coords[0]][coords[1]] = backup
+            attempts -= 1
+
+    return grid
+
+
 
 def runAlgorithm():
 
@@ -120,14 +169,14 @@ def runAlgorithm():
     grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
     grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-    returned = solveSudoku(grid)
 
-    print(grid)
+    fillSudoku(grid)
+
+
+    finishedGrid = main(grid)
 
     #timetaken returns 0 - algorithm too quick
     #number of passes returns 0 ????
-
-    return returned
 
 
 if __name__ == "__main__":
