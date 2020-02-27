cc5559d731ca7b0688e5ad5181db3750aa5e46a1
Calum Harvey <calum.harvey@blueyonder.co.uk>
2020-02-27 16:13:07 +0000

    fixed sudoku gen
    it now works

--- sudokuGen.py
+++ sudokuGen.py
@@ -61,24 +61,22 @@ class sudokuGen:
         return False
 
 
-    def solveSudoku(self, board, start, end):
+    def solveSudoku(self, board, nums):
 
         pos = [0, 0]
 
         if(not self.findUnassignedLocation(board, pos)):
-            # print("solution Found")
             return True
 
         # might need this
         row = pos[0]
         col = pos[1]
 
-        for num in range(start, end):
+        for num in nums:
             if(self.noConflicts(board, pos, num)):
                 board[row][col] = num
 
-                if(self.solveSudoku(board, start, end)):
-                    # print("solution Found 2")
+                if(self.solveSudoku(board, nums)):
                     return True
 
                 board[row][col] = 0
@@ -136,56 +134,36 @@ class sudokuGen:
 
 
     def main(self, grid):
-        attempts = 5
+        # 5 = very easy
+        # 15 = easy
+        # 30 = easy
+        # 100 = hard
+        attempts = 50
+        asc = [1,2,3,4,5,6,7,8,9]
+        desc = [9,8,7,6,5,4,3,2,1]
 
         while(attempts > 0):
+            print(attempts)
 
             copyGrid1 = []
             copyGrid2 = []
-            updatedBoard = []
 
             #Select a random non-empty cell from the board and set it to zero, storing the number as a backup
             coords = self.selectEmptyCell(grid)
             backup = grid[coords[0]][coords[1]]
             grid[coords[0]][coords[1]] = 0
 
-
-            print("coords" , coords)
-            print(grid[coords[0]][coords[1]])
-            print("Grid\n", grid)
-
             #Create copy of the grid and try and solve the puzzle
-            copyGrid1 = copy.copy(grid)
-            print("grid1", grid)
-            copyGrid2 = copy.copy(grid)
-            print("grid2", grid)
-            self.updatedBoard = grid
-            print("grid3", grid)
-            print("updatedBoard1" , self.updatedBoard)
-            solvedAsc = self.solveSudoku(copyGrid1, 1, 10)
-            print("grid4", grid)
-            print("updatedBoard2", self.updatedBoard)
-            solvedDesc = self.solveSudoku(copyGrid2, 10, 1)
-            print("grid5", grid)
-
-
-            #print(solvedAsc)
-            #print("copygrid1\n" , copyGrid1)
-            #print(solvedDesc)
-            #print("copygrid2\n", copyGrid2)
-            #print(np.array_equal(copyGrid1, copyGrid2))
-            #input()
-
+            copyGrid1 = copy.deepcopy(grid)
+            copyGrid2 = copy.deepcopy(grid)
+            self.solveSudoku(copyGrid1, asc)
+            self.solveSudoku(copyGrid2,desc)
 
             if(not np.array_equal(copyGrid1, copyGrid2)):
                 #print("run")
                 grid[coords[0]][coords[1]] = backup
                 attempts -= 1
 
-            #Grid at end is completed but shouldnt be 
-            print("grid at end\n" , grid)
-
-            input()
         return grid
 
 
