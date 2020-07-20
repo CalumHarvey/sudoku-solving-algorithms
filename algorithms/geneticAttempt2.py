19c7d81297c220de61a0fa1db003c9398f916670
Calum Harvey <calum.harvey@blueyonder.co.uk>
2020-07-20 18:05:44 +0100

    mutation and population sorting added

--- algorithms/geneticAttempt2.py
+++ algorithms/geneticAttempt2.py
@@ -9,7 +9,7 @@ class Candidate:
         self.fitness = None
     
     def getBox(self, boxNum):
-        ''' Given box number from 0-8 return a list of the coordinates of the box '''
+        """ Given box number from 0-8 return a list of the coordinates of the box """
         #Get coordinate of first row and column of box number
         firstRow = (boxNum // 3) * 3
         firstCol = (boxNum % 3) * 3
@@ -20,6 +20,7 @@ class Candidate:
         return boxCoords
     
     def initialise(self, puzzle):
+        """ Intialise a candidate solution by filling in blank spaces in incomplete puzzle given"""
 
         populatedBoard = puzzle.copy()
         #For each box number 1-9...
@@ -51,38 +52,130 @@ class Candidate:
 
 
     def updateFitness(self):
-        pass
+        """ Update candidate fitness (number of errors) """
+
+        score = 0
+        
+        #For each row 1-9...
+        for x in range(9):
+            
+            #Intialise sets
+            rowSet = set()
+            colSet = set()
+            
+            #For each value in row...
+            for y in range(9):
+                
+                #Add numbers from each row into rowSet and numbers in columns into colSet
+                rowSet.add(self.values[x][y])
+                colSet.add(self.values[y][x])
+
+            #Add to score the number of values in each row and column that is less than 9 (all the values)
+            score += (9 - len(rowSet))
+            score += (9 - len(colSet))       
+        
+        self.fitness = score
+
+
+    def mutate(self, mutationRate, originalPuzzle):
+        """ Picking a box, and then picking 2 values within the box to swap"""
+
+        r = random.uniform(0, 1.1)
+
+        if(r > mutationRate):
+
+            while True:
+                #Get random box number and get list of coordinates in that box
+                boxNum = random.randrange(9)
+                boxCoords = self.getBox(boxNum)
+                print(boxNum)
+
+                #Get list of cells in box that are not in original board and can be changed
+                changeableCells = [i for i in boxCoords if originalPuzzle[i[0]][i[1]] == 0]
+
+                if(len(changeableCells) >= 2):
+                    break
+        
+            #Pick 2 random coordinates from the list of changeable cells
+            a = random.sample(changeableCells, 1)
+            b = random.sample(changeableCells, 1)
+        
+            #Swap values of 2 random coordinates
+            self.values[a[0][0]][a[0][1]], self.values[b[0][0]][b[0][1]] = self.values[b[0][0]][b[0][1]] , self.values[a[0][0]][a[0][1]]
+
+
+
 
 
 
 class Population:
+    """ Population consists of a set of candidate solutions to the puzzle"""
     def __init__(self):
         self.candidates = []
     
     def intialise(self, puzzle):
+        """ Initialise all the candidate solutions in the population """
         
         for x in range(100):
             newCandidate = Candidate()
             newCandidate.initialise(puzzle)
-            print(newCandidate.values)
-            input()
             self.candidates.append(newCandidate)
 
 
     
     def updateFitnesses(self):
+        """ Update all the fitnesses within the population """
+
+        for candidate in self.candidates:
+            candidate.updateFitness()
+
+
+    def sort(self):
+        """ Sort all the candidate solutions by their fitness from lowest to highest """
+        self.candidates.sort(key=lambda x: x.fitness)
         pass
 
-    def getBest(self):
+
+
+
+
+
+class Selection:
+
+
+    def compete(self, candidates):
         pass
+
+
+
+
+class Crossover:
+
+    def crossover(self, parent1, parent2, crossoverRate):
+        pass
+
+
+    def crossoverBoxes(self):
+        pass
+
+
 
 
 if __name__ == "__main__":
+
+    mutation_rate = 0.06
 
     board = np.array([[7, 0, 6, 1, 3, 2, 0, 9, 0], [0, 0, 2, 6, 7, 4, 0, 3, 0], [0, 0, 1, 0, 0, 9, 0, 2, 0], [0, 4, 0, 9, 0, 0, 1, 0, 2], [2, 0, 9, 3, 0, 7, 4, 0, 6], [1, 0, 0, 0, 0, 5, 3, 8, 9], [3, 0, 0, 0, 0, 6, 2, 1, 0], [0, 1, 0, 2, 4, 3, 0, 6, 5], [6, 0, 0, 7, 0, 1, 9, 4, 0]])
 
     P = Population()
     P.intialise(board)
+
+    for c in P.candidates:
+        print(c.values)
+        input()
+        c.mutate(mutation_rate, board)
+        print(c.values)
+        input()
 
 
     
